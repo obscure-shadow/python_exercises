dc6a683ad5f53d763b9f33287a5b8933ba3fdc15
David Paul <dfp333@gmail.com>
2018-07-11 11:08:57 -0500

    finished unit testing, test passed. more testing after lunch

--- testing/test_intro.py
+++ testing/test_intro.py
@@ -1,6 +1,13 @@
 import unittest
 
+def fun(num):
+    return num
+
 class My_Test(unittest.TestCase):
-    def test(self):
-        self.assertEqual(fun(), 8)
+    #always name tests test_* and be descriptive of what it does
+    def test_fun_returns_8(self):
+        self.assertEqual(fun(8), 8)
+
+if __name__ == '__main__':
+    unittest.main()
 
