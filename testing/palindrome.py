13def76391e42bf919ba1e3ea57fac2aee037a51
David Paul <dfp333@gmail.com>
2018-07-12 09:32:59 -0500

    initialized animal testing

---
+++ testing/palindrome.py
@@ -0,0 +1,6 @@
+def fun(num):
+    return num
+
+class Palindrome():
+    def is_palindrome(self):
+        return True
--- testing/test_intro.py
+++ testing/test_intro.py
@@ -1,12 +1,19 @@
 import unittest
+import palindrome
+from palindrome import Palindrome
 
 def fun(num):
     return num
 
-class My_Test(unittest.TestCase):
+class Palindrome_Test(unittest.TestCase):
     #always name tests test_* and be descriptive of what it does
     def test_fun_returns_8(self):
-        self.assertEqual(fun(8), 8)
+        self.assertEqual(palindrome.fun(8), 8)
+
+    def test_for_palindrome(self):
+        pal = Palindrome()
+        self.assertTrue(palindrome.is_palindrome())
+
 
 if __name__ == '__main__':
     unittest.main()
